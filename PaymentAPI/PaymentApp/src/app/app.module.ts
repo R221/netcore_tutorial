b9cfbc11edf5a8c25739c43fe4d404f77be8a08d
Robert James <rcjames1111@gmail.com>
2021-10-16 07:00:51 -0700

    Created a validated Angular form with bootstrap

---
+++ PaymentAPI/PaymentApp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetailFormComponent } from './payment-details/payment-detail-form/payment-detail-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ PaymentAPI/PaymentApp/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
+
+@Component({
+  selector: 'app-payment-detail-form',
+  templateUrl: './payment-detail-form.component.html',
+  styles: [
+  ]
+})
+export class PaymentDetailFormComponent implements OnInit {
+
+  public results: any;
+  constructor(public service:PaymentDetailService) { }
+
+  ngOnInit(): void {
+  }
+
+}
